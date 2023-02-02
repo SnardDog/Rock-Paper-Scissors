5664bfc5dc844a7fdb0b2d1e6f6487b143bcbc44
SnardDog <SnardDog@proton.me>
2023-02-02 00:02:41 -0600

    Game now ends if player/comp has a tally of 3

--- rpc.js
+++ rpc.js
@@ -5,95 +5,95 @@ let playerTally = 0;
 let compTally = 0;
 
 function getCompChoice() {
-  compChoice = choiceOptions[Math.floor(Math.random() * choiceOptions.length)];
-  console.log(compChoice);
-  return compChoice;
+	compChoice = choiceOptions[Math.floor(Math.random() * choiceOptions.length)];
+	console.log(compChoice);
+	return compChoice;
 }
 
 function getPlayerChoice() {
-  playerChoice = prompt("Rock, Paper, Scissors?");
-  console.log(playerChoice);
-  return playerChoice;
+	playerChoice = prompt("Rock, Paper, Scissors?");
+	console.log(playerChoice);
+	return playerChoice;
 }
 
 function gameRound() {
-  getPlayerChoice();
-  getCompChoice();
-  if (playerChoice.toLowerCase() == compChoice.toLowerCase()) {
-    console.log("It's a draw!");
-  } else if (
-    playerChoice.toLowerCase() == "rock" &&
-    compChoice.toLowerCase() == "paper"
-  ) {
-    console.log("Paper beats Rock! You Lose!");
-    compTally++;
-  } else if (
-    playerChoice.toLowerCase() == "rock" &&
-    compChoice.toLowerCase() == "scissors"
-  ) {
-    console.log("Rock beats Scissors! You win!");
-    playerTally++;
-  } else if (
-    playerChoice.toLowerCase() == "paper" &&
-    compChoice.toLowerCase() == "rock"
-  ) {
-    console.log("Paper beats rock! You win!");
-    playerTally++;
-  } else if (
-    playerChoice.toLowerCase() == "paper" &&
-    compChoice.toLowerCase() == "scissors"
-  ) {
-    console.log("Scissors beats Paper! You lose!");
-    compTally++;
-  } else if (
-    playerChoice.toLowerCase() == "scissors" &&
-    compChoice.toLowerCase() == "rock"
-  ) {
-    console.log("Rock beats Scissors! You lose!");
-    compTally++;
-  } else if (
-    playerChoice.toLowerCase() == "scissors" &&
-    compChoice.toLowerCase() == "paper"
-  ) {
-    console.log("Scissors beats paper! You win!");
-    playerTally++;
-  }
+	getPlayerChoice();
+	getCompChoice();
+	if (playerChoice.toLowerCase() == compChoice.toLowerCase()) {
+		console.log("It's a draw!");
+	} else if (
+		playerChoice.toLowerCase() == "rock" &&
+		compChoice.toLowerCase() == "paper"
+	) {
+		console.log("Paper beats Rock! You Lose!");
+		compTally++;
+	} else if (
+		playerChoice.toLowerCase() == "rock" &&
+		compChoice.toLowerCase() == "scissors"
+	) {
+		console.log("Rock beats Scissors! You win!");
+		playerTally++;
+	} else if (
+		playerChoice.toLowerCase() == "paper" &&
+		compChoice.toLowerCase() == "rock"
+	) {
+		console.log("Paper beats rock! You win!");
+		playerTally++;
+	} else if (
+		playerChoice.toLowerCase() == "paper" &&
+		compChoice.toLowerCase() == "scissors"
+	) {
+		console.log("Scissors beats Paper! You lose!");
+		compTally++;
+	} else if (
+		playerChoice.toLowerCase() == "scissors" &&
+		compChoice.toLowerCase() == "rock"
+	) {
+		console.log("Rock beats Scissors! You lose!");
+		compTally++;
+	} else if (
+		playerChoice.toLowerCase() == "scissors" &&
+		compChoice.toLowerCase() == "paper"
+	) {
+		console.log("Scissors beats paper! You win!");
+		playerTally++;
+	} 
 }
 
 function tallyCount() {
-  if (playerTally > compTally) {
-    console.log(
-      "Your score was " +
-        playerTally +
-        " and the computers score was " +
-        compTally +
-        ". You win!"
-    );
-  } else if (playerTally < compTally) {
-    console.log(
-      "Your score was " +
-        playerTally +
-        " and the computers score was " +
-        compTally +
-        ". You lose!"
-    );
-  } else {
-    console.log(
-      "Your score was " +
-        playerTally +
-        " and the computers score was " +
-        compTally +
-        ". It's a draw!"
-    );
-  }
+	if (playerTally > compTally) {
+		console.log(
+			"Your score was " +
+				playerTally +
+				" and the computers score was " +
+				compTally +
+				". You win!"
+		);
+	} else if (playerTally < compTally) {
+		console.log(
+			"Your score was " +
+				playerTally +
+				" and the computers score was " +
+				compTally +
+				". You lose!"
+		);
+	} else {
+		console.log(
+			"Your score was " +
+				playerTally +
+				" and the computers score was " +
+				compTally +
+				". It's a draw!"
+		);
+	}
 }
 
 function game() {
-  for (let i = 0; i < 5; i++) {
-    gameRound();
-    console.log(playerTally, compTally);
-  }
-  tallyCount();
+	for (let i = 0; i < 5; i++ || playerTally == 3 || compTally == 3) {
+		gameRound();
+		console.log(playerTally, compTally);
+	}
+	tallyCount();
 }
 
 game();
